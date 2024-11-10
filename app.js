ea06698c8a20a9a7c5721f4e523e9e3cb28522a0
Sergey Belyakov <39454357+srgblkv@users.noreply.github.com>
2024-11-11 09:07:34 +1000

    Update app.js

--- app.js
+++ app.js
@@ -8,27 +8,27 @@ class Car {
     constructor(make, model, run) {
         this.#make = make;
         this.#model = model;
-        this.#run = this.setRun(run);
+        this.run = run;
     }
 
     info() {
-        console.log(`${this.#make} ${this.#model} ${this.getRun()}`);
+        console.log(`${this.#make} ${this.#model} ${this.#run}`);
     }
 
-    getRun() {
+    get run() {
         return this.#run;
     }
 
-    setRun(run) {
+    set run(run) {
         if (run < 0) {
-            return 0;
+            return this.#run = 0
         }
         return this.#run = run;
     }
 }
 
-const newCar = new Car('Porshe', '911', '-10000');
+const newCar = new Car('Porshe', '911', '10000');
 
 newCar.info();
-newCar.setRun(123124515261);
-console.log(newCar.getRun());
+newCar.run = -15;
+console.log(newCar.run);
